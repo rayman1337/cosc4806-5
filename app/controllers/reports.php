c2c03913eb08ee9791f2eb32c0ef4e6c28160bc3
RM <134272733+rayman1337@users.noreply.github.com>
2025-07-15 10:44:06 +0000

    Implemented getDeleted notes method in reports controller

--- app/controllers/reports.php
+++ app/controllers/reports.php
@@ -13,13 +13,16 @@ class Reports extends Controller {
         $userModel = $this->model('User');
 
         $allNotes = $noteModel->getAll();
+        $deletedNotes = $noteModel->getDeleted();
         $mostNotesUser = $noteModel->getUserWithMostNotes();
         $loginCounts = $userModel->getLoginCounts();
 
         $this->view('reports/index', [
             'notes' => $allNotes,
+            'deleted' => $deletedNotes,
             'mostNotesUser' => $mostNotesUser,
             'logins' => $loginCounts
         ]);
+
     }
 }
